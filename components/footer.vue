58d586d86761cde38c83d3c3c8faa1c4f2acbe5b
Chien <vanchien882000@gmail.com>
2021-07-23 10:49:25 +0700

    add footer

---
+++ components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="contain-footer">
+        <div class="left">
+          <img src="../assets/images/footer/license.png">
+          <img src="../assets/images/footer/sabs-1.png">
+          <img src="../assets/images/footer/curacao-1.png">
+          <img src="../assets/images/footer/itech-1.png">
+        </div>
+        <div class="right">
+          <div class="copy-right">Copyright © 2020 bplay.com. All rights reserved.</div>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss">
+  .footer {
+    @import '../assets/css/footer.scss';
+  }
+</style>
