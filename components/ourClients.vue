cb54c83ba282beb515e2db450658afeed839ff5e
Chien <vanchien882000@gmail.com>
2021-07-23 10:50:03 +0700

    add ourClient

---
+++ components/ourClients.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="our-clients" id="our-clients">
+        <div class="container-content">
+            <div class="sub-title">BPLAY GAME</div>
+            <div class="title">OUR CLIENTS</div>
+        </div>
+        <div class="list-card">
+            <div class="card">
+                <img src="../assets/images/ourClients/icon1.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon2.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon3.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon4.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon5.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon6.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon7.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon8.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon9.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon10.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon11.png">
+            </div>
+            <div class="card">
+                <img src="../assets/images/ourClients/icon12.png">
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+  .our-clients {
+    @import '../assets/css/ourClients.scss';
+  }
+</style>
